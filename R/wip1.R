fe050fb0318f4fa853643575dbc020ad0abae4be
Sara Thiebaud <sara.e.thiebaud@gmail.com>
2015-07-21 10:51:33 -0700

    Initial toy project for SISBID5, with files

---
+++ R/wip1.R
@@ -0,0 +1,29 @@
+#' Print a message to the terminal.
+#'
+#' More interesting description of what the function does.  Blah blah.
+#'
+#' @param None Function doesn't take any arguments, yet.
+#'
+#' @return None Function prints to terminal but doesn't return anything.
+#'
+#' @examples
+#' baby_function()
+#'
+#' @seealso
+#' The general \code{\link{print}} function.
+#'
+#' @export
+#'
+
+baby_function <- function() {
+  print("This is a trivial function that prints to the terminal!")
+}
+
+
+#' A package that analyzes wip1 data pulled from TumorPortal.
+#'
+#' More description of package goes here.
+#' @docType package
+#' @name wip1
+#'
+NULL
